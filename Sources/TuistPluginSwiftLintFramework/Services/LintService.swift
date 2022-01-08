5a55813395716b274b1858da760615c17dcfb257
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2022-01-08 19:24:25 +0100

    WIP: `LintService`.

--- Sources/TuistPluginSwiftLintFramework/Services/LintService.swift
+++ Sources/TuistPluginSwiftLintFramework/Services/LintService.swift
@@ -1,4 +1,5 @@
 import Foundation
+import ProjectAutomation
 
 /// A service that manages code linting.
 public final class LintService {
@@ -9,6 +10,21 @@ public final class LintService {
     ///   - path: The path to the directory that contains the workspace or project whose code will be linted.
     ///   - targetName: The target to be linted. When not specified all the targets of the graph are linted.
     public func run(path: String?, targetName: String?) throws {
-        print("Start liniting!")
+        print("Hello")
+        
+        do {
+            let graph: Graph = try {
+                if let path = path {
+                    return try Tuist.graph(at: path)
+                } else {
+                    return try Tuist.graph()
+                }
+            }()
+        } catch {
+            print(error)
+        }
+        
+        //        let targets = graph.projects.values.flatMap(\.targets)
+        //        print("The current graph has the following targets: \(targets.map(\.name).joined(separator: " "))")
     }
 }
