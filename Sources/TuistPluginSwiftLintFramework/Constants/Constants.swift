e3b1fd420ee0a17af1b9ffdbe854b1572e663d91
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2022-02-26 21:48:40 +0100

    Bump version.

--- Sources/TuistPluginSwiftLintFramework/Constants/Constants.swift
+++ Sources/TuistPluginSwiftLintFramework/Constants/Constants.swift
@@ -3,5 +3,5 @@ import Foundation
 /// A set of constant values that can be reused across `TuistPluginSwiftLintFramework` module.
 enum Constants {
     /// The plugin version.
-    static let version = "0.0.1"
+    static let version = "0.1.0"
 }
