4a2fa8040c7c1a8d0ce8268e9268775b7710b5dd
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2022-03-17 13:52:13 +0100

    Stamp a new version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ Please, check out guidelines: https://keepachangelog.com/en/1.0.0/
 
 ## [Unreleased]
 
+## 0.2.0
+
 ### Changed
  
 - `SwiftLint` dependency has been updated to `0.46.5` (was `0.46.1`).
--- Sources/TuistPluginSwiftLintFramework/Constants/Constants.swift
+++ Sources/TuistPluginSwiftLintFramework/Constants/Constants.swift
@@ -3,5 +3,5 @@ import Foundation
 /// A set of constant values that can be reused across `TuistPluginSwiftLintFramework` module.
 enum Constants {
     /// The plugin version.
-    static let version = "0.1.0"
+    static let version = "0.2.0"
 }
