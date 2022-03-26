b825454a1f186d53cb9020c1c14038ef4beb2361
fortmarek <marekfort@me.com>
2022-03-26 21:48:22 +0100

    Point to ProjectAutomation repository

---
+++ fixtures/Example1/Tuist/Config.swift
@@ -0,0 +1,7 @@
+import ProjectDescription
+
+let config = Config(
+    plugins: [
+        // .git(url: "https://github.com/tuist/tuist-plugin-lint", tag: "0.2.0") // Uncomment to test released plugin and specify the desired version
+    ]
+)
